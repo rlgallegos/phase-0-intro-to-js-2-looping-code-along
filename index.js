ea0fae37e5afb93d98db03183cde02a70be96eaf
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-09 18:02:27 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function countDown(n)
+{
+    for (let i = n; i >= 0; i--)
+    {
+        console.log(i);
+    }
+}
+
+function writeCards(names, eventName)
+{
+    const messages = [];
+    for (let i = 0; i < names.length; i++)
+    {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
